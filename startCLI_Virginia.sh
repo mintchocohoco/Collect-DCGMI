9c8ea67591075c9c648f7ebd5125502160a1fb96
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 20:39:32 +0900

    Update startCLI_Virginia.sh

--- startCLI_Virginia.sh
+++ startCLI_Virginia.sh
@@ -19,14 +19,14 @@ echo $INSTANCE_DNS
 
 # Instance setting
 # sleep 60
-# AWS_KEY="ys.pem"
+AWS_KEY="ys.pem"
 # echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Collect-DCGMI/&& python3.7 workload.py'
 #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Collect-DCGMI/&& python3.7 workload.py'
 
 # # Run Experiments
 # sleep 60
-AWS_KEY="ys.pem"
+#AWS_KEY="ys.pem"
 # echo 'start experiment'
 #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& python3.7 workload.py'
 #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 
