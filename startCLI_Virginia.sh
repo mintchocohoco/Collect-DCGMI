99615a30b95da5a54729b942683a332c61c2342c
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 16:20:27 +0900

    Update startCLI_Virginia.sh

--- startCLI_Virginia.sh
+++ startCLI_Virginia.sh
@@ -22,14 +22,14 @@ sleep 60
 AWS_KEY="ys.pem"
 echo 'git clone and setting instance'
 ssh -o "StrictHostKeyChecking no" -i $AWS_KEY ubuntu@$INSTANCE_DNS 'git clone https://github.com/hyoonseo159357/Hardware-Data-Collect.git'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./settings.sh'
+ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& sudo bash ./settings.sh && python3.7 workload.py'
 
-# Run Experiments
-sleep 60
-AWS_KEY="ys.pem"
-echo 'start experiment'
-ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& python3.7 workload.py'
-#ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 
+# # Run Experiments
+# sleep 60
+# AWS_KEY="ys.pem"
+# echo 'start experiment'
+# #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 'cd /home/ubuntu/Hardware-Data-Collect/&& python3.7 workload.py'
+# #ssh -i $AWS_KEY -t ubuntu@$INSTANCE_DNS 
 
 # # Get results
 # sleep 10
