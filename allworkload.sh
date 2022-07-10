676f9d5646361d9f291d21ee24f507447fa90c4a
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 14:52:11 +0900

    Update allworkload.sh

--- allworkload.sh
+++ allworkload.sh
@@ -2,7 +2,9 @@ for var1 in 32 64 128 224 256
   do
     for var2 in 16 32 64 128 256
     do
-      for var3 in 'LeNet5' 'VGGSmall' 'VGG19' 'ResNetSmall' 'MNIST_CNN' 'CIFAR10_CNN' 'AlexNet' 'InceptionV3' 'InceptionResNetV2' 'Xception' 'EfficientNetB0' 'MobileNetV2' 'ResNet50'
+      #for var3 in 'LeNet5' 'VGGSmall' 'VGG19' 'ResNetSmall' 'MNIST_CNN' 'CIFAR10_CNN' 'AlexNet' 'InceptionV3' 'InceptionResNetV2' 'Xception' 'EfficientNetB0' 'MobileNetV2' 'ResNet50'
+      for var3 in 'LeNet5' 'VGGSmall' 'VGG19' 'VGG13' 'VGG16' 'VGG11' 'ResNetSmall' 'MNIST_CNN' 'CIFAR10_CNN' 'AlexNet' 'InceptionV3' 'InceptionResNetV2' 'Xception' 'EfficientNetB0' 'MobileNetV2' 'ResNet50' 'ResNet18' 'ResNet34'     
+
       do
         python3.7 workload.py --model $var3 --dataset $var1 --batch_size $var2
       done
