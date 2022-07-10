5e09db89bf8cdd74825aaedf03bec11b4baa99e4
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 17:21:39 +0900

    Update settings.sh

--- settings.sh
+++ settings.sh
@@ -20,3 +20,6 @@ sudo dpkg -i cuda-keyring_1.0-1_all.deb
 sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
 sudo apt-get update && sudo apt-get install -y datacenter-gpu-manager
 sudo systemctl --now enable nvidia-dcgm
+
+# train
+python3.7 workload.py
