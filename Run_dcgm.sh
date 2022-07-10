c58d6024e1e8f838d88cd34ef89f7b63afbddcd7
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 15:04:15 +0900

    필요없는 파일 삭제

--- Run_dcgm.sh
+++
@@ -1,6 +0,0 @@
-chmod +x allworkload.sh
-chmod +x dcgmi_field1.sh
-chmod +x dcgmi_field2.sh
-chmod +x dcgmi_field3.sh
-
-nohup ./allworkload.sh >nohup.out & nohup ./dcgmi_field1.sh >nohup2.out & nohup ./dcgmi_field2.sh >nohup3.out & nohup ./dcgmi_field3.sh >nohup4.out &
