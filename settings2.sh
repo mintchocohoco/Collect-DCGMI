9c06217342c8d25487ef59ad9a27c2142551208d
hyoonseo159357 <50091802+hyoonseo159357@users.noreply.github.com>
2022-07-10 23:48:18 +0900

    Create settings2.sh

---
+++ settings2.sh
@@ -0,0 +1,24 @@
+pip3 install tensorflow==2.7.0
+
+sudo apt update
+sudo killall apt apt-get
+sudo rm /var/lib/apt/lists/lock
+sudo rm /var/cache/apt/archives/lock
+sudo rm /var/lib/dpkg/lock*
+sudo dpkg --configure -a
+sudo apt update
+
+# DSTAT 설치 + 권한설정
+sudo apt install dstat
+chmod +x dcgmi_field.sh
+
+# DCGMI설치
+wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/cuda-keyring_1.0-1_all.deb
+sudo dpkg -i cuda-keyring_1.0-1_all.deb
+sudo add-apt-repository "deb http://developer.download.nvidia.com/compute/cuda/repos/ubuntu1804/x86_64/ /"
+sudo apt-get update && sudo apt-get install -y datacenter-gpu-manager
+sudo systemctl --now enable nvidia-dcgm
+
+# train
+git clone https://github.com/hyoonseo159357/Collect-DCGMI.git
+python3.7 workload.py
